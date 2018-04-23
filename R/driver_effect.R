0098e852e7f165372fa51e38dbe8469f4a15bdfb
Brian Yandell <byandell@wisc.edu>
2018-04-23 12:06:48 -0500

    add driver_effect

---
+++ R/driver_effect.R
@@ -0,0 +1,56 @@
+#' Driver effect on target and mediator
+#' 
+#' Compare driver effect on mediator, target and target adjusted by mediator.
+#' 
+#' @param object data frame with effects
+#' @param driver_levels levels of driver
+#' 
+driver_effect <- function(out, driver_levels = LETTERS[1:8]) {
+  out1 <- out[, c("target","group","mediator","pvalue",
+                  paste0(driver_levels, "_m"),
+                  driver_levels,
+                  paste0(driver_levels, "_p"))]
+  out1 <- 
+    dplyr::arrange(
+      dplyr::ungroup(
+        dplyr::mutate(
+          dplyr::group_by(
+            dplyr::mutate(
+              tidyr::gather(
+                out1,
+                driver_level, effect, -target, -group, -mediator, -pvalue),
+              fitType = stringr::str_remove(driver_level, "[A-H]_*"),
+              driver_level = stringr::str_remove(driver_level, "_[a-z]"),
+              fitType = ifelse(fitType == "", "a", fitType),
+              fitType = c(m="Mediator", p="Target", a="Adjusted")[fitType],
+              fitType = factor(fitType, c("Mediator", "Target", "Adjusted"))),
+            target, group, mediator, fitType),
+          effect = effect - mean(effect))),
+      pvalue)
+  class(out1) <- c("driver_effect", class(out1))
+  out1
+}
+ggplot_driver_effect <- function(out1,
+                              colors = qtl2::CCcolors) {
+  driver_names <- names(colors)
+  names(driver_names) <- sort(unique(out1$driver_level))
+  out1 <- 
+    dplyr::mutate(
+      dplyr::filter(
+        dplyr::mutate(
+          out1,
+          mediator = paste0(mediator, " (", signif(pvalue, 2), ")")),
+        mediator %in% unique(mediator)[1:12]),
+      driver_level = driver_names[driver_level],
+      mediator = factor(mediator, unique(mediator)))
+  ggplot2::ggplot(out1) +
+    ggplot2::aes(fitType, effect, color = driver_level, group = driver_level) +
+    ggplot2::geom_line() +
+    ggplot2::geom_point(size=2) +
+    ggplot2::facet_wrap(~ mediator) +
+    ggplot2::ggtitle(paste(out1$target[1], "for group", out1$group[1])) +
+    ggplot2::scale_color_manual(name = "driver",
+                                values = colors)
+  
+}
+autoplot.driver_effect <- function(...) ggplot_driver_effect(...)
