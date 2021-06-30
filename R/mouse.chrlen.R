4dd14c093552956f2497a01c3dcc81961ec83b95
Brian Yandell <byandell@wisc.edu>
2021-06-30 18:43:34 -0500

    add resized Tmem68 data and manual pages

---
+++ R/mouse.chrlen.R
@@ -0,0 +1,21 @@
+#' Mouse chromosome lengths
+#'
+#' Lengths in base pairs of the 22 autosomes for mouse.
+#'
+#' @docType data
+#'
+#' @usage data(mouse.chrlen)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Chick et al. (2016) 
+#' (\href{https://dx.org/10.1038/nature18270}{Nature 534: 500-505}) 
+#'
+#' @source \href{https://github.com/churchill-lab/intermediate}{Churchill GitHub}
+#'
+#' @examples
+#' data(mouse.chrlen)
+#' mouse.chrlen
+"mouse.chrlen"
